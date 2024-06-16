56027394394994bd340646345051054dbcea68dc
barok-dell <77874161+barok-dell@users.noreply.github.com>
2024-06-15 23:08:59 -0700

    static files
    static files added and configured

--- car_inventory_system/settings.py
+++ car_inventory_system/settings.py
@@ -116,9 +116,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-
+# If you have a 'static' directory at the project level
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
